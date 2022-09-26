5e0fca53cccf9e434b2ae846bc83d72cb92197d3
widouglas <widouglasmail@gmail.com>
2022-09-26 16:16:09 -0300

    Correcao do Use Teste

--- tests/Unit/CustomerTest.php
+++ tests/Unit/CustomerTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit;
 
+use App\Models\Customer;
+use App\Repositories\Eloquent\CustomerRepository;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
